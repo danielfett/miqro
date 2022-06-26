cabd2da3e52758a48e76d43bcf913ebef12d0b7d
Daniel Fett <fett@danielfett.de>
2022-06-26 14:42:48 +0100

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -18,5 +18,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'paho.mqtt',
+        'yaml',
+    ],
     python_requires='>=3.6',
 )
